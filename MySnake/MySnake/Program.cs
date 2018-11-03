d13502555c043e774a4a3126eb626db82c730ffc
anvolkravt <anvolkravt@ya.ru>
2018-11-04 02:46:21 +0300

    Hello World

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,6 +10,8 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();       
         }
     }
 }
