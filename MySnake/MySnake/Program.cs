3b8955515c4cf3a6f2c55991be493b22fa70ffdf
anvolkravt <anvolkravt@ya.ru>
2018-11-08 03:55:02 +0300

    Class Snake is added

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -22,13 +22,12 @@ namespace MySnake
             rightLine.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
  
         }
-
-      
-       
+               
     }
 }
